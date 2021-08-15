d14137bd62a066bec924c6d7759e1e496449321b
Rishu <rishabhsinghcs1@gamil.com>
2021-08-15 14:09:58 +0530

    second commit

--- placement/settings.py
+++ placement/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-iyd#e=i8!3ep@wlu07b&!hs90&0d&1d@=s83y$3hwb#dfblenh'
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'django-insecure-iyd#e=i8!3ep@wlu07b&!hs90&0d&1d@=s83y$3hwb#dfblenh'
+# SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
